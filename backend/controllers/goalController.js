3019b2fc148804e1699782d9f4378eaa896c6494
TaoJacqueline <tao.jacqueline858@gmail.com>
2022-05-11 11:39:15 +0200

    error middleware + async handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,29 +1,35 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get goals
 // @route GET /api/goals
 // @acces private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: 'Get goals' });
-};
+});
 
 // @desc Set goal
 // @route POST /api/goals
 // @acces private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
+	if (!req.body.text) {
+		res.status(400);
+		throw new Error('Please add a text field');
+	}
 	res.status(200).json({ message: 'Set goals' });
-};
+});
 
 // @desc update goal
 // @route PUT /api/goals/:id
 // @acces private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // @desc delete goal
 // @route DELETE /api/goals/:id
 // @acces private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = { getGoals, setGoals, updateGoal, deleteGoal };
