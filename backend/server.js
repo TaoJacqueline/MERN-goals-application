7df02182ca9a3776361c78ea46ae70c139ad3cf6
TaoJacqueline <tao.jacqueline858@gmail.com>
2022-05-11 13:32:15 +0200

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
@@ -12,4 +16,4 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log('listening on port ' + port));
+app.listen(port, () => console.log(`Listenning on port : ${port}`.yellow));
