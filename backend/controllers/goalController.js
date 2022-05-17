9c1f90ce55f93585439da4c47c914cd39e73992b
TaoJacqueline <tao.jacqueline858@gmail.com>
2022-05-17 10:42:28 +0200

    auth ! JWt backend

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 // @desc Get goals
 // @route GET /api/goals
 // @acces private
 const getGoals = asyncHandler(async (req, res) => {
-	const goals = await Goal.find();
+	const goals = await Goal.find({ user: req.user.id });
 
 	res.status(200).json(goals);
 });
@@ -22,6 +23,7 @@ const setGoals = asyncHandler(async (req, res) => {
 
 	const goal = await Goal.create({
 		text: req.body.text,
+		user: req.user.id,
 	});
 	res.status(200).json(goal);
 });
@@ -36,6 +38,20 @@ const updateGoal = asyncHandler(async (req, res) => {
 		throw new Error('goal not found');
 	}
 
+	const user = await User.findById(req.user.id);
+
+	//Check for user
+	if (!user) {
+		res.status(401);
+		throw new Error('user not found');
+	}
+
+	// Make sure the loggged in user matches the goal user
+
+	if (goal.user.toString() !== user.id) {
+		res.status(401);
+		throw new Error('user not authorized');
+	}
 	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 	});
@@ -52,6 +68,21 @@ const deleteGoal = asyncHandler(async (req, res) => {
 	if (!goal) {
 		res.status(400);
 		throw new Error('goal not found');
+	}
+
+	const user = await User.findById(req.user.id);
+
+	//Check for user
+	if (!user) {
+		res.status(401);
+		throw new Error('user not found');
+	}
+
+	// Make sure the loggged in user matches the goal user
+
+	if (goal.user.toString() !== user.id) {
+		res.status(401);
+		throw new Error('user not authorized');
 	}
 	await goal.deleteOne();
 	res.status(200).json({ id: req.params.id });
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
 			_id: user.id,
 			name: user.name,
 			email: user.email,
+			token: generateToken(user.id),
 		});
 	} else {
 		res.status(400);
@@ -49,14 +50,36 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @acces public
 const loginUser = asyncHandler(async (req, res) => {
-	res.json({ message: 'login User' });
+	const { email, password } = req.body;
+
+	//check for user email
+	const user = await User.findOne({ email });
+
+	if (user && (await bcrypt.compare(password, user.password))) {
+		res.json({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user.id),
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid credentials');
+	}
 });
 
 // @desc get user data
 // @route GET /api/users/me
-// @acces public
+// @acces private
 const getMe = asyncHandler(async (req, res) => {
-	res.json({ message: 'User data display' });
+	const { _id, name, email } = await User.findById(req.user.id);
+
+	res.status(200).json({ id: _id, name, email });
 });
+
+// Generate JWT
+const generateToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
 
 module.exports = { registerUser, loginUser, getMe };
